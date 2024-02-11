fdee9f8ad754754f4795aed49efac2ca1e7c79bd
Ahmed Elmanzlawy <ahmed.abdelnaser.elmanzlawy@gmail.com>
2024-02-11 09:33:54 +0200

    fixed

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import Movie from "./components/Movie/Movie";
 import Favourites from "./pages/Favourites";
 import Navbar from "./components/Navbar/Navbar";
 import MovieDetails from "./pages/MovieDetails";
-// import SearchBar from "./components/Search/SearchBar";
 
 function App() {
   return (
     <div className="min-vh-100" style={{backgroundColor:"#2f2f2f"}} >
       <BrowserRouter>
       <Navbar />
-      {/* <SearchBar /> */}
         <Routes>
           <Route path="/" element={<Movie />} />
           <Route path="/movie/:id" element={<MovieDetails />} />
